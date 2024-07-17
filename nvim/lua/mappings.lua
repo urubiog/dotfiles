64377fb0e4d0b38863efab0fb49130b622fad0d4
Uriel Rubio García <159381070+urubiog@users.noreply.github.com>
2024-07-17 10:51:43 +0200

    Added funcionallity to nvim with dashboard plugin

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -14,6 +14,16 @@ map('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent =
 -- Signature help 
 map("n", "<F3>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
 
+-- Lsp diagnostic 
+map("n", "<F4>", "<cmd>lopen<CR>", { noremap = true, silent = true })
+
+-- Indentation and "<",">"
+map("n", "<F5>", "<cmd><<CR>")
+map("n", "<F6>", "<cmd>><CR>")
+map("v", "<F5>", "<")
+map("v", "<F6>", ">")
+
+
 -- Formaters
 -- map("n", "<C-u>", "<cmd>!black %<CR><CR>")
 map("n", "<C-y>", "<cmd>lua vim.lsp.buf.format()<CR>")
@@ -29,3 +39,4 @@ map("n", "_", "<cmd>Dashboard<CR>")
 
 cmd("lua require('lazygit')")
 cmd("colorscheme nvchad")
+cmd("highlight Visual guibg=#808080 guifg=#ffffff ctermbg=4 ctermfg=15")
